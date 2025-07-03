bfea9c1522d57f22f6000cd66a6fb68682a986ed
muhammadsarim <muhammadsarim>
2025-07-03 08:13:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// let btn = document.getElementById("mobile-nav")
+// console.log();
+
+// btn.addEventListener(("click", () => {
+// btn.innerHTML = `
+//  <nav class="navitems">
+//       <ul>
+//         <li><a href="#">Gallery</a></li>
+//         <li><a href="#">Prices for services</a></li>
+//         <li><a href="#">About us</a></li>
+//         <li><a href="#">Contact</a></li>
+//       </ul>
+//     </nav>
+// `
+
+// }))
